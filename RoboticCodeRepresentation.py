00fc4847325ca2a978fbb2f018bf19f25791877e
wwilczyn <wojtek.wilczynski@gmail.com>
2024-04-15 16:08:16 +0200

    Initial commit

---
+++ RoboticCodeRepresentation.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, cmd_name, left_node=None, right_node=None):
+        self.command_name = cmd_name
+        self.left_node = left_node
+        self.right_node = right_node
+
+    def get_value(self):
+        return self.command_name
+
+
+file = open("RCR_commands.txt", mode="r")
+commands = []
+for line in file:
+    commands.append(file.readline())
+
+
+print(commands)
+
+
